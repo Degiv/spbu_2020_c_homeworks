86b8f85e9e455a05121038c181a5387880b2af28
Ivan Degtyarev <st051982@student.spbu.ru>
2020-10-17 00:31:07 +0300

    fix code style

--- homework_4/hw4.3/main.c
+++ homework_4/hw4.3/main.c
@@ -19,11 +19,9 @@ int* getBinary(int number)
     int *binary =  calloc(sizeof(int) * byteSize, sizeof(int));
     unsigned char *bytes = (unsigned char*)&number;
 
-    for (int i = numOfBytes - 1; i >= 0; --i)
-    {
+    for (int i = numOfBytes - 1; i >= 0; --i) {
         int mask = 0x80;
-        for (int j = 0; j < byteSize; ++j)
-        {
+        for (int j = 0; j < byteSize; ++j) {
             if (mask & bytes[i])
                 binary[(numOfBytes - i - 1) * byteSize + j] = 1;
             else
@@ -35,23 +33,20 @@ int* getBinary(int number)
     return binary;
 }
 
-int* sum(int *a, int *b)
+int* sum(int* a, int* b)
 {
     int *sum = calloc(sizeof(int) * numOfBytes, sizeof(int));
     sum[sizeof(int) * byteSize - 1] = 0;
-    for (int i = sizeof(int) * byteSize - 1; i >= 0; --i)
-    {
+    for (int i = sizeof(int) * byteSize - 1; i >= 0; --i) {
         sum[i] += a[i] + b[i];
         if (i != 0)
             sum[i - 1] = sum[i] / 2;
         sum[i] %= 2;
     }
 
-    if (a[0] == 1 && b[0] == 1)
-    {
+    if (a[0] == 1 && b[0] == 1) {
         int i = 0;
-        for (i = sizeof(int) * byteSize - 1; sum[i] == 0; --i)
-        {
+        for (i = sizeof(int) * byteSize - 1; sum[i] == 0; --i) {
             sum[i] = 1;
         }
         sum[i] = 0;
@@ -64,8 +59,7 @@ int getDecimal(int* binary)
 {
     int decimal = 0;
     bool isNegative = binary[0];
-    for (int i = 1; i < sizeof(int) * byteSize; ++i)
-    {
+    for (int i = 1; i < sizeof(int) * byteSize; ++i) {
         if (isNegative)
             binary[i] = (binary[i] + 1) % 2;
         decimal += binary[i] * exponention(2, sizeof(int) * byteSize - i - 1);
@@ -75,7 +69,7 @@ int getDecimal(int* binary)
     return decimal;
 }
 
-void showBinary(int *binary)
+void showBinary(int* binary)
 {
     for (int i = 0; i < sizeof(int) * byteSize; ++i) {
         printf("%d", binary[i]);
@@ -90,14 +84,14 @@ int main()
     printf("Enter two numbers:\n");
     scanf("%d %d", &a, &b);
 
-    int *binaryA = getBinary(a);
-    int *binaryB = getBinary(b);
+    int* binaryA = getBinary(a);
+    int* binaryB = getBinary(b);
     printf("Binary code of first number is: ");
     showBinary(binaryA);
     printf("Binary code of second number is: ");
     showBinary(binaryB);
 
-    int *binarySum = sum(binaryA, binaryB);
+    int* binarySum = sum(binaryA, binaryB);
     printf("Binary code of sum: ");
     showBinary(binarySum);
 
